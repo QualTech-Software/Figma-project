8fb3970c8d2b8d3cf82adc533e4ffb46e412b584
Shravani K <shravani.qualtech@gmail.com>
2024-02-01 17:21:10 +0530

    TaskId:jk36sHH4 initial commit

---
+++ Figma-shravani/component/src/Components/About.jsx
@@ -0,0 +1,42 @@
+ export const cardData = [
+  {
+    name: "AARON HARKER",
+    location: "New York, USA",
+    description:
+      "We have a very complex contractor safety and risk management platform and QualTech is our go to IT support firm.",
+    image:
+      "https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=612x612&w=0&k=20&c=QjebAlXBgee05B3rcLDAtOaMtmdLjtZ5Yg9IJoiy-VY=",
+  },
+  {
+    name: "JANE SMITH",
+    location: "London, UK",
+    description:
+      "QualTech has exceeded my expectations in every way. They deeply care about their client's needs and are committed to the end result.",
+    image:
+      "https://i.pinimg.com/474x/98/51/1e/98511ee98a1930b8938e42caf0904d2d.jpg",
+  },
+  {
+    name: "Alice Johnson",
+    location: "Sydney, Australia",
+    description:
+      "QualTech has exceeded my expectations in every way. They deeply care about their client's needs and are committed to the end result.",
+    image:
+      "https://pics.craiyon.com/2023-07-15/dc2ec5a571974417a5551420a4fb0587.webp",
+  },
+  {
+    name: "Bob Williams",
+    location: "Tokyo, Japan",
+    description:
+      "QualTech has exceeded my expectations in every way. They deeply care about their client's needs and are committed to the end result.",
+    image:
+      "https://a.storyblok.com/f/191576/1200x800/faa88c639f/round_profil_picture_before_.webp",
+  },
+  {
+    name: "Eva Brown",
+    location: "Berlin, Germany",
+    description:
+      "QualTech has exceeded my expectations in every way. They deeply care about their client's needs and are committed to the end result.",
+    image:
+      "https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-600nw-1714666150.jpg",
+  },
+];
