638132283e84f5ec5b5488c646dbafd6906f2fd3
shukratamore <shukrata.qualtech@gmail.com>
2024-02-08 16:07:15 +0530

    TaskId:aF3Wf92B initial commit

---
+++ qualtech/src/Component/ServiceCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ServiceCard({ title, description, svgImages }) {
+  return (
+    <div className="card">
+      <div className="svg-container">
+        {svgImages.map((image, index) => (
+          <React.Fragment key={index}>
+            <img src={image.src} alt={image.alt} className={image.className} />
+            <p className={`svg-label ${image.labelClass}`}>{image.label}</p>
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="card-data">
+        <div className="title">{title}</div>
+        <div className="text">{description}</div>
+        <div className="btn">
+          <p>Explore</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ServiceCard;
--- qualtech/src/Pages/Home.jsx
+++ qualtech/src/Pages/Home.jsx
@@ -1,36 +1,33 @@
 import React from "react";
-import Vector from '../assets/Vector.svg';
-import Arrow from '../assets/Arrow 2.svg';
-import Design from '../assets/Designcomp.svg';
+import { Vector, Arrow } from "../assets";
 
-function Home() {
-    return (
-        <>
-            <div className="homecontainer">
-                <img src={Vector} className="vector" />
-                <div className="Title">
-                    <h1>Growing Businesses through Digital Transformation.</h1>
-                </div>
-                <div className="Text">
-                    <p>QualTech has been established with the vision of providing a software
-                        platform to the business, societal sections lacking the benefits of
-                        technology. We are a trusted partner in digital engineering and enterprise
-                        modernization, and our services provide  distinct competitive advantage to
-                        our customers.</p>
-                </div>
-                <button className="button"><p>Contact Us Now</p></button>
-                <button className="dservice">
-                    <p>
-                        Discover Our Services
-                        <img src={Arrow} className="arrow" />
-                    </p>
-                </button>
-                <div className="designs">
-                <img src={Design} className="design" />
-                </div>
-            </div>
-        </>
-    );
-}
+const Home = () => (
+  <>
+    <div className="homecontainer">
+      <img src={Vector} className="vector" />
+      <div className="Title">
+        <h1>Growing Businesses through Digital Transformation.</h1>
+      </div>
+      <div className="Text">
+        <p>
+          QualTech has been established with the vision of providing a software
+          platform to the business, societal sections lacking the benefits of
+          technology. We are a trusted partner in digital engineering and
+          enterprise modernization, and our services provide distinct
+          competitive advantage to our customers.
+        </p>
+      </div>
+      <button className="button">
+        <p>Contact Us Now</p>
+      </button>
+      <button className="dservice">
+        <p>
+          Discover Our Services
+          <img src={Arrow} className="arrow" />
+        </p>
+      </button>
+    </div>
+  </>
+);
 
 export default Home;
